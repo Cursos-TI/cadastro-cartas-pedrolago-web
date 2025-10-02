039cb99370ee98b3749d6f492fc085c7c37ecccb
pedrolago-web <pedrolago2001@gmail.com>
2025-10-02 18:35:59 +0000

    dados7

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -61,3 +61,24 @@ int main() {
   printf("Numero de Pontos Turisticos: ");
   scanf("%d", &pontosTuristicos2);
 
+  // Área para exibição dos dados da cidade
+
+  printf("\n=== Informacoes das Cartas: ===\n");
+
+  printf("\nCarta 1:\n");
+  printf("Estado: %c\n", estado1);
+  printf("Codigo: %s\n", codigo1);
+  printf("Nome da Cidade: %s\n", nomeCidade1);
+  printf("Populacao: %d\n", populacao1);
+  printf("Area: %.2f km2\n", area1);
+  printf("PIB: %.2f bilhoes de reais\n", pib1);
+  printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos1);
+
+  printf("\nCarta 2:\n");
+  printf("Estado: %c\n", estado2);
+  printf("Codigo: %s\n", codigo2);
+  printf("Nome da Cidade: %s\n", nomeCidade2);
+  printf("Populacao: %d\n", populacao2);
+  printf("Area: %.2f km2\n", area2);
+  printf("PIB: %.2f bilhoes de reais\n", pib2);
+  printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
