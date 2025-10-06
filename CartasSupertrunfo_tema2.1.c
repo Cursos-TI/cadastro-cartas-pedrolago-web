efe5868ea537e31827d7cb3645b1ba1c269c7d08
pedrolago-web <pedrolago2001@gmail.com>
2025-10-06 13:13:29 +0000

    correção

--- CartasSupertrunfo_tema2.1.c
+++ CartasSupertrunfo_tema2.1.c
@@ -11,20 +11,20 @@ int main() {
     // Carta 1
     char estado1;
     char codigo1[4];
-    char nomeCidade1[50];
+    char cidade1[50];
     int populacao1;
     float area1;
     float pib1;
-    int pontosTuristicos1;
+    int pontos1
     
     // Carta 2
     char estado2;
     char codigo2[4];
-    char nomeCidade2[50];
+    char cidade2[50];
     int populacao2;
     float area2;
     float pib2;
-    int pontosTuristicos2;
+    int pontos2;
     
   // Área para entrada de dados
 
